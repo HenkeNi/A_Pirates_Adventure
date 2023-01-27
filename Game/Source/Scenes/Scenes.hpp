46f77098a1279aa8e5dbc6579d2403a315520e44
Henrik Jangefelt <Henke9105@hotmail.com>
2023-01-27 21:57:13 +0100

    include file for all scenes

---
+++ Game/Source/Scenes/Scenes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// maybe rename SceneIncl?
+
+#include "Game/GameScene.h"
+#include "Loading/LoadingScene.h"
+#include "Menu/MenuScene.h"
+#include "Pause/PauseScene.h"
+#include "Title/TitleScene.h"
