85f4632e175fbcc43ae063e1665aacdd380f89ba
Henrik Jangefelt <Henke9105@hotmail.com>
2023-01-27 21:54:08 +0100

    Updated Game class, registering scenes

--- Game/Source/Game.cpp
+++ Game/Source/Game.cpp
@@ -1,26 +1,50 @@
 #include "Pch.h"
 #include "Game.h"
+#include "Scenes/Scenes.hpp"
 
 
+Game::Game()
+{
+}
 
-
-
-
-
-
-
+Game::~Game()
+{
+}
 
 void Game::OnUpdate(float aDeltaTime)
 {
-	std::cout << "Updating Game\n";
+	m_sceneManager.Update(aDeltaTime);
 }
 
-void Game::OnRender()
+void Game::OnLateUpdate(float aDeltaTime)
+{
+	m_sceneManager.LateUpdate(aDeltaTime);
+}
+
+void Game::OnDraw()
+{
+	m_sceneManager.Draw();
+}
+
+void Game::OnCreate()
+{
+	SetupScenes();
+}
+
+void Game::OnDestroy()
 {
 
 }
 
 void Game::SetupScenes()
 {
+	SceneManagerProxy proxy{ m_sceneManager };
 
+	m_sceneManager.Register(std::make_unique<TitleScene>(proxy),	eSceneType::Title);
+	m_sceneManager.Register(std::make_unique<LoadingScene>(proxy),  eSceneType::Loading);
+	m_sceneManager.Register(std::make_unique<MenuScene>(proxy),		eSceneType::Menu);
+	m_sceneManager.Register(std::make_unique<GameScene>(proxy),		eSceneType::Game);
+	m_sceneManager.Register(std::make_unique<PauseScene>(proxy),	eSceneType::Pause);
+
+	m_sceneManager.Init({ eSceneType::Game, eSceneType::Menu, eSceneType::Loading, eSceneType::Title });
 }
--- Game/Source/Game.h
+++ Game/Source/Game.h
@@ -1,16 +1,23 @@
 #pragma once
-#include <Hi_Engine.h>
-
+#include "SceneManager.h"
 
 class Game : public Hi_Engine::Application
 {
 public:
-	void OnUpdate(float aDeltaTime)		override;
-	void OnRender()		override;
+	Game();
+	~Game();
+
+	void			OnUpdate(float aDeltaTime)		override;
+	void			OnLateUpdate(float aDeltaTime)	override;
+	void			OnDraw()						override;
+
+	void			OnCreate()						override;
+	void			OnDestroy()						override;
 
 private:
-	void SetupScenes();
+	void			SetupScenes();
 
+	SceneManager	m_sceneManager;
 };
 
 Hi_Engine::Application* Hi_Engine::CreateApplication()
