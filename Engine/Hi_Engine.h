ca28bc9ac3426e6437f62273533aa6bb67e293fd
Henrik Jangefelt <Henke9105@hotmail.com>
2023-01-27 21:49:57 +0100

    added some includes

--- Engine/Hi_Engine.h
+++ Engine/Hi_Engine.h
@@ -2,3 +2,8 @@
 
 #include "Core/Application/Application.h"
 #include "Core/EntryPoint.h"
+#include "Utility/DataStructures/Linear/Dynamic/Stack/Stack.hpp"
+#include "Utility/DataStructures/Linear/Static/Array/VectorOnStack.hpp"
+#include "Utility/Math/Vectors/Vector.hpp"
+#include "Utility/Math/Matrices/Matrix3x3.hpp"
+#include "Utility/Math/Matrices/Matrix4x4.hpp"
