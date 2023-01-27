dd451997fdf78468789e1a8456bc8cde18998560
Henrik Jangefelt <Henke9105@hotmail.com>
2023-01-27 21:56:23 +0100

    Implemented a scene manager

---
+++ Game/Source/Managers/SceneManager.cpp
@@ -0,0 +1,108 @@
+#include "Pch.h"
+#include "SceneManager.h"
+#include "../Scenes/Scene.h"
+
+
+SceneManager::SceneManager()
+	: m_scenes{ nullptr }
+{
+}
+
+SceneManager::~SceneManager()
+{
+	for (auto& scene : m_scenes)
+		scene->OnDestroyed();
+	
+	Clear();
+}
+
+void SceneManager::Init(std::initializer_list<eSceneType> aList)
+{
+	for (auto& sceneType : aList)
+		m_sceneStack.Push(sceneType);
+}
+
+void SceneManager::Register(std::unique_ptr<Scene> aScene, eSceneType aType)
+{
+	assert(aScene != nullptr);
+
+	int index = (int)aType;
+
+	m_scenes[index] = std::move(aScene);
+	m_scenes[index]->OnCreated();
+}
+
+void SceneManager::Push(eSceneType aType)
+{
+	if (!IsEmpty())
+	{
+		m_scenes[(int)m_sceneStack.Top()]->OnExit();
+	}
+
+	m_sceneStack.Push(aType);
+	m_scenes[(int)m_sceneStack.Top()]->OnEnter();
+}
+
+void SceneManager::Pop()
+{
+	if (!IsEmpty())
+	{
+		m_scenes[(int)m_sceneStack.Top()]->OnExit();
+		m_sceneStack.Pop();
+
+		m_scenes[(int)m_sceneStack.Top()]->OnEnter();
+	}
+}
+
+void SceneManager::SwapTo(eSceneType aType)
+{
+	if (!IsEmpty())
+	{
+		m_scenes[(int)m_sceneStack.Top()]->OnExit();
+		m_sceneStack.Pop();
+	}
+
+	m_sceneStack.Push(aType);
+	m_scenes[(int)m_sceneStack.Top()]->OnEnter();
+}
+
+void SceneManager::Update(float aDeltaTime)
+{
+	if (!IsEmpty()) 
+	{  
+		int index = (int)m_sceneStack.Top();
+		m_scenes[index]->Update(aDeltaTime);	
+	}
+}
+
+void SceneManager::LateUpdate(float aDeltaTime)
+{
+	if (!IsEmpty()) 
+	{
+		int index = (int)m_sceneStack.Top();
+		m_scenes[index]->LateUpdate(aDeltaTime);
+	}
+}
+
+void SceneManager::Draw() const
+{
+	if (IsEmpty()) 
+	{ 
+		int index = (int)m_sceneStack.Top();
+
+		if (m_scenes[index]->IsTransparent())
+			m_scenes[index - 1]->Draw();
+
+		m_scenes[index]->Draw();
+	}
+}
+
+bool SceneManager::IsEmpty() const
+{
+	return m_sceneStack.IsEmpty();
+}
+
+void SceneManager::Clear()
+{
+	m_sceneStack.Clear();
+}
---
+++ Game/Source/Managers/SceneManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "../Data/Enumerations.h"
+#include <Hi_Engine.h>
+
+namespace CU = CommonUtilities;
+
+class Scene;
+
+class SceneManager
+{
+public:
+	SceneManager();
+	~SceneManager();
+
+	void Init(std::initializer_list<eSceneType> aList);
+	void Register(std::unique_ptr<Scene> aScene, eSceneType aType);
+	void Push(eSceneType aType);
+	void Pop();
+	void SwapTo(eSceneType aType);
+
+	void Update(float aDeltaTime);	
+	void LateUpdate(float aDeltaTime);
+	void Draw()														const;
+	bool IsEmpty()													const;
+	void Clear();
+
+private:
+	std::array<std::unique_ptr<Scene>, (int)eSceneType::Count>		m_scenes;
+	CU::Stack<eSceneType>											m_sceneStack;
+};
+
+// TODO: Add ProcessEvents() ??
