884059fbeaefeb81bee2af01de60fb04ce4a25bc
Henrik Jangefelt <Henke9105@hotmail.com>
2023-03-05 22:11:40 +0100

    Empty functions for collision manager

--- Engine/Source/Core/Physics/Collisions/CollisionManager.cpp
+++ Engine/Source/Core/Physics/Collisions/CollisionManager.cpp
@@ -1,2 +1,40 @@
 #include "Pch.h"
 #include "CollisionManager.h"
+
+
+namespace Hi_Engine
+{
+	CollisionManager::CollisionManager()
+	{
+	}
+
+	CollisionManager::~CollisionManager()
+	{
+	}
+
+	void CollisionManager::HandleCollisions()
+	{
+	}
+
+	void CollisionManager::AddCollider(C_Collider* aCollider)
+	{
+	}
+
+	void CollisionManager::RemoveCollider(C_Collider* aCollider)
+	{
+	}
+
+	void CollisionManager::CheckCollisions()
+	{
+	}
+
+	void CollisionManager::ResolveCollision(C_Collider* aCollider1, C_Collider* aCollider2)
+	{
+	}
+
+	bool CollisionManager::Contains(C_Collider* aCollider)
+	{
+		return false;
+	}
+
+}
