2b9565f78dbd84ceb26d16c78e87c67ec9f72824
Henrik Jangefelt <Henke9105@hotmail.com>
2023-03-05 22:15:27 +0100

    Added some more includes

--- Engine/Hi_Engine.h
+++ Engine/Hi_Engine.h
@@ -1,12 +1,23 @@
 #pragma once
 
+/* - Core - */
 #include "Core/Application/Application.h"
 #include "Core/EntryPoint.h"
+
+/* - Rendering - */
+#include "Core/Rendering/Renderers/SpriteRenderer/SpriteRenderer.h"
+
+/* - Data Structures - */
 #include "Utility/DataStructures/Linear/Dynamic/Stack/Stack.hpp"
+#include "Utility/DataStructures/Linear/Dynamic/Queue/Queue.hpp"
 #include "Utility/DataStructures/Linear/Static/Array/VectorOnStack.hpp"
 #include "Utility/DataStructures/Linear/Static/MemoryPool/MemoryPool.hpp"
+
+/* - Math - */
 #include "Utility/Math/Vectors/Vector.hpp"
 #include "Utility/Math/Matrices/Matrix3x3.hpp"
 #include "Utility/Math/Matrices/Matrix4x4.hpp"
+
+/* - Utility - */
 #include "Utility/Utility/UtilityFunctions.hpp"
 
