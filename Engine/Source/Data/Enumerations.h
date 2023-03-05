aaf54b5d8ccd29cac427f3df5582a2c38a5b6499
Henrik Jangefelt <Henke9105@hotmail.com>
2023-03-05 22:15:40 +0100

    Updated POD

--- Engine/Source/Data/Enumerations.h
+++ Engine/Source/Data/Enumerations.h
@@ -6,9 +6,29 @@ enum class ePriority
 	Low, Moderate, High, NotSpecified
 };
 
-enum class eWindowEventType
+enum class eWindowEvent
 {
 	Resize, LostFocus, GainedFocus, Close
+};
+
+enum class eInput
+{
+	Key_Enter,
+	Key_Space,
+	Key_Escape,
+	Key_W,
+	Key_A,
+	Key_S,
+	Key_D,
+	Key_P,
+	Key_ArrowUp,
+	Key_ArrowDown,
+	Key_ArrowLeft,
+	Key_ArrowRight,
+	Mouse_LeftBtn,
+	Mouse_RightBtn,
+	Mouse_Moved,
+	Count
 };
 
 enum class eKeyState
@@ -16,3 +36,7 @@ enum class eKeyState
 	Press, Release, Repeat
 };
 
+enum class eCameraMovement
+{
+	Forward, Backward, Left, Right
+};
--- Engine/Source/Data/Structs.h
+++ Engine/Source/Data/Structs.h
@@ -4,59 +4,94 @@
 
 namespace CU = CommonUtilities;
 
-
-struct WindowData
+namespace Hi_Engine
 {
-	CU::Vector2<unsigned>	m_size;
-	std::string				m_name, m_iconPath;
-};
+	struct WindowData
+	{
+		CU::Vector2<unsigned>	m_size;
+		std::string				m_name, m_iconPath;
+	};
+
+	struct CameraData
+	{
+		CU::Vector3<float>	m_position;
+		CU::Vector3<float>	m_front, m_up, m_right; // make struct for coordinate system?
+		CU::Vector3<float>	m_worldUp;
+
+		float				m_movmentSpeed, m_zoom;
+	};
 
 
-// Put in Game??
-class InputHandler;
-class Window;
 
-struct SharedContext 
-{
-	// SceneManagerProxy	m_sceneManager;
-	// Window&				m_window;
-	InputHandler&		m_inputHandler;
-};
-
-
-//class TimeObserver; // RENAME??
-
-struct TimerRequest // RENAME??
-{
-	double					m_durationInSecs, m_timeOfRequest;
-	std::function<void()>	m_callback = nullptr;
+	class Texture2D;
 	
-	//TimeObserver*			m_observer = nullptr;
-};
+	struct SpriteRenderData
+	{
+		Texture2D&			m_texture;
+		CU::Vector4<float>	m_color;
+		CU::Vector3<float>	m_position;
+		CU::Vector2<float>	m_size;			// Rename Scale??
+		float				m_rotation;
+	};
+
+
+	// Put in Game??
+	class InputHandler;
+	class Window;
+
+	struct SharedContext
+	{
+		// SceneManagerProxy	m_sceneManager;
+		// Window&				m_window;
+		InputHandler& m_inputHandler; // Pass around?? Or do everytihng with events??
+		// Timer& m_timer; // ?? or a proxy??
+	};
 
 
 
 
-//??
-//struct Position
-//{
-//
-//	union 
-//	{
-//		struct
-//		{
-//			float x, y;
-//		};
-//
-//		CU::Vector2<float> vec2;
-//	};
-//
-//	//union 
-//	//{
-//	//	CU::Vector2<float> m_position,
-//	//	float x, float y
-//	//};
-//
-//
-//
-//};
+
+
+
+	//class TimeObserver; // RENAME??
+
+	struct TimerRequest // RENAME??
+	{
+		double					m_durationInSecs, m_timeOfRequest;
+		std::function<void()>	m_callback = nullptr;
+
+		//TimeObserver*			m_observer = nullptr;
+	};
+
+
+
+
+
+
+
+
+	//??
+	//struct Position
+	//{
+	//
+	//	union 
+	//	{
+	//		struct
+	//		{
+	//			float x, y;
+	//		};
+	//
+	//		CU::Vector2<float> vec2;
+	//	};
+	//
+	//	//union 
+	//	//{
+	//	//	CU::Vector2<float> m_position,
+	//	//	float x, float y
+	//	//};
+	//
+	//
+	//
+	//};
+}
+
