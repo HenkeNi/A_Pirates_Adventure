34c2ae5e0f0abc471e92ce895464ac3cb3e4674f
Henrik Jangefelt <Henke9105@hotmail.com>
2023-04-04 23:56:41 +0200

    Map class for rendering tiles

--- Game/Source/World/Map/Map.cpp
+++ Game/Source/World/Map/Map.cpp
@@ -1,2 +1,42 @@
 #include "Pch.h"
 #include "Map.h"
+
+
+
+#include "Core/Resources/ResourceHolder.hpp"
+
+Map::Map()
+	: m_mapWidth{ 16 }, m_mapHeight{ 16 }, m_tileSize{ 32 }, m_texture{ nullptr }
+{
+}
+
+void Map::Init()
+{
+	m_texture = &Hi_Engine::ResourceHolder<Hi_Engine::Texture2D>::GetInstance().GetResource("floor");
+}
+
+void Map::Draw() const
+{
+	CU::Vector2<unsigned> tileCoord;
+
+	// Draw map
+	for (int height = 0; height < m_mapHeight; ++height)
+	{
+		for (int width = 0; width < m_mapWidth; ++width)
+		{
+			tileCoord.x = width  * m_tileSize;
+			tileCoord.y = height * m_tileSize;
+
+			tileCoord.x /= 1400;
+			tileCoord.y /= 800;
+
+
+			// Draw tile..
+			Hi_Engine::SpriteRenderer::GetInstance().Render({ *m_texture, m_tilePrototype.m_color, {1 , 1, -1.f }, m_tilePrototype.m_scale, m_tilePrototype.m_rotation });
+			Hi_Engine::SpriteRenderer::GetInstance().Render({ *m_texture, m_tilePrototype.m_color, { (float)tileCoord.x + m_tileSize, (float)tileCoord.y, -1.f }, m_tilePrototype.m_scale, m_tilePrototype.m_rotation });
+		
+			return;
+		}
+	}
+
+}
--- Game/Source/World/Map/Map.h
+++ Game/Source/World/Map/Map.h
@@ -1,14 +1,33 @@
 #pragma once
 
+// Look at instancing...
 
 // TileMap??
 class Map
 {
 public:
+	Map();
+
+	void Init();
+	void Draw() const;
 
 private:
+	struct Tile
+	{
+		CU::Vector4<float> m_color = { 1.f, 1.f, 1.f, 1.f };
+		CU::Vector2<float>	m_scale = { 1.f, 1.f };
+		float				m_rotation = 0;
+	} m_tilePrototype;
+
 	// tiles... -> opengl instancing..??
 
 	// vector of ints? tile types... 
+
+
+	unsigned m_mapWidth, m_mapHeight;	
+	unsigned m_tileSize;
+
+	// Texture...
+	Hi_Engine::Texture2D* m_texture;
 };
 
