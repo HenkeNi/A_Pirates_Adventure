604aba9c03d5a45eeb672a34eb784d6d3e48eec1
Henrik Jangefelt <Henke9105@hotmail.com>
2023-04-10 21:58:33 +0200

    Updated with destroy function

--- Engine/Source/Core/Messaging/Events/SystemEvents/SystemEvents.cpp
+++ Engine/Source/Core/Messaging/Events/SystemEvents/SystemEvents.cpp
@@ -1,20 +1,35 @@
 #include "Pch.h"
 #include "SystemEvents.h"
 #include "../../Listener/EventListener.h"
-
+#include "../../../../Utility/DataStructures/Linear/Static/MemoryPool/MemoryPool.hpp"
 
 namespace Hi_Engine
 {
 #pragma region WINDOW_EVENT
 
-	WindowEvent::WindowEvent(eWindowEvent aType)
-		: Event{ ePriority::High }, m_eventType{ aType }
+	WindowEvent::WindowEvent()
+		: Event{ ePriority::Moderate }, m_eventType{ eWindowEvent::None }, m_windowSize{ 0, 0 }
+	{
+	}
+
+	WindowEvent::~WindowEvent()
 	{
 	}
 
 	void WindowEvent::Dispatch(EventListener& aListener)
 	{
 		aListener.HandleEvent(*this);
+	}
+
+	void WindowEvent::Destroy()
+	{
+		CU::MemoryPool<WindowEvent>::GetInstance().ReturnResource(this);
+	}
+	
+	void WindowEvent::Init(eWindowEvent aType)
+	{
+		m_eventType = aType;
+		m_isHandled = false;
 	}
 
 	eWindowEvent WindowEvent::GetEventType() const
@@ -25,17 +40,41 @@ namespace Hi_Engine
 #pragma endregion WINDOW_EVENT
 
 
-
 #pragma region KEY_EVENT
 
 	KeyEvent::KeyEvent()
-		: Event{ ePriority::High }
+		: Event{ ePriority::High }, m_keyState{ eKeyState::None }, m_keyCode{ -1 } 
+	{
+	}
+
+	KeyEvent::~KeyEvent()
 	{
 	}
 
 	void KeyEvent::Dispatch(EventListener& aListener)
 	{
 		aListener.HandleEvent(*this);
+	}
+
+	void KeyEvent::Destroy()
+	{
+		CU::MemoryPool<KeyEvent>::GetInstance().ReturnResource(this);
+	}
+
+	void KeyEvent::Init(eKeyState aState, int aCode)
+	{
+		m_keyState = aState;
+		m_keyCode = aCode;
+	}
+
+	eKeyState KeyEvent::GetKeyState() const
+	{
+		return m_keyState;
+	}
+
+	int	KeyEvent::GetKeyCode() const
+	{
+		return m_keyCode;
 	}
 
 #pragma endregion KEY_EVENT
@@ -45,13 +84,33 @@ namespace Hi_Engine
 #pragma region MOUSE_EVENT
 
 	MouseEvent::MouseEvent()
-		: Event{ ePriority::High }
+		: Event{ ePriority::Low }, m_currMousePos{ 0, 0 }, m_prevMousePos{ 0, 0 }
+	{
+	}
+
+	MouseEvent::~MouseEvent()
 	{
 	}
 
 	void MouseEvent::Dispatch(EventListener& aListener)
 	{
+		aListener.HandleEvent(*this);
+	}
 
+	void MouseEvent::Destroy()
+	{
+		CU::MemoryPool<MouseEvent>::GetInstance().ReturnResource(this);
+	}
+
+	void MouseEvent::Init(const CU::Vector2<unsigned>& aCurrentMousePos, const CU::Vector2<unsigned>& aPrevMousePos)
+	{
+		m_currMousePos = aCurrentMousePos;
+		m_prevMousePos = aPrevMousePos;
+	}
+
+	const CU::Vector2<unsigned>& MouseEvent::GetCurrentMousePos() const
+	{
+		return m_currMousePos;
 	}
 
 #pragma endregion MOUSE_EVENT
--- Engine/Source/Core/Messaging/Events/SystemEvents/SystemEvents.h
+++ Engine/Source/Core/Messaging/Events/SystemEvents/SystemEvents.h
@@ -10,14 +10,17 @@ namespace Hi_Engine
 	class WindowEvent : public Event
 	{
 	public:
-		WindowEvent(eWindowEvent aType); // Overload new operatp`?? placement new??
+		WindowEvent();			// Overload new operatp`?? placement new?? 
+		~WindowEvent();
 
-		void Dispatch(EventListener& aListener) override;
+		void					Dispatch(EventListener& aListener)	override;
+		void					Destroy()							override;
+		void					Init(eWindowEvent aType);
 
-		eWindowEvent GetEventType() const;
+		eWindowEvent			GetEventType()						   const;
 
 	private:
-		eWindowEvent		m_eventType;
+		eWindowEvent			m_eventType;
 		CU::Vector2<unsigned>	m_windowSize;
 	};
 
@@ -27,28 +30,40 @@ namespace Hi_Engine
 	{
 	public:
 		KeyEvent();
+		~KeyEvent();
 
-		void	Dispatch(EventListener& aListener) override;
+		void		Dispatch(EventListener& aListener)   override;
+		void		Destroy()							 override;
+		void		Init(eKeyState aState, int aCode);
+
+		eKeyState	GetKeyState()						    const;
+		int			GetKeyCode()						    const;
 
 	private:
-		eKeyState m_keyState;
+		eKeyState	m_keyState;
+		int			m_keyCode;
 	};
 
 
 
+	// OR KEy mesage and mouse messag (current pos, last pos, maybe keypressed as well (rather tha having to listen for a key)) or combine it all and send a Input Event?
+	// Maybe store relevant keys here as well??
 	class MouseEvent : public Event
 	{
-		// OR KEy mesage and mouse messag (current pos, last pos, maybe keypressed as well (rather tha having to listen for a key)) or combine it all and send a Input Event?
-		// Maybe store relevant keys here as well??
 	public:
 		MouseEvent();
+		~MouseEvent();
 
-		void	Dispatch(EventListener& aListener) override;
+		void	Dispatch(EventListener& aListener)	override;
+		void	Destroy()							override;
+		void	Init(const CU::Vector2<unsigned>& aCurrentMousePos, const CU::Vector2<unsigned>& aPrevMousePos);
+
+		const CU::Vector2<unsigned>& GetCurrentMousePos() const;
 
 	private:
 		// Store under struct MouseData??
-		CU::Vector2<unsigned>	m_currentMousePos, m_previousMousePos;
-		bool					m_leftMouseCliked; // STATE?
+		CU::Vector2<unsigned>	m_currMousePos, m_prevMousePos;
+		bool					m_leftMouseCliked; // STATE? Clicked, Held, Releasd?	map for different buttons??
 	};
 
 }
