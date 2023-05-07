e878bc4db6d157ddef98a52f8adc043d1a57ef19
Henrik Jangefelt <Henke9105@hotmail.com>
2023-05-08 01:04:53 +0200

    Added some new members to gameworld

--- Game/Source/World/GameWorld.h
+++ Game/Source/World/GameWorld.h
@@ -1,5 +1,16 @@
 #pragma once
+
+class Map;
+class Area;
+
 class GameWorld
 {
+public:
+
+
+private:
+	std::vector<Area*> m_areas;	// areas, regions, locations, levels
+
+	Map* m_map; // Possibly by value..
 };
 
