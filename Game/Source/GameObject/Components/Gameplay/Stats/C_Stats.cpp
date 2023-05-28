8d7c9862b9678e71f6513a7824aff47614312f1e
Henrik Jangefelt <Henke9105@hotmail.com>
2023-05-28 02:22:35 +0200

    Renmad class

--- Game/Source/GameObject/Components/Gameplay/Stats/C_Stats.cpp
+++ Game/Source/GameObject/Components/Gameplay/Stats/C_Stats.cpp
@@ -1,2 +1,12 @@
 #include "Pch.h"
 #include "C_Stats.h"
+
+
+C_Stats::C_Stats(GameObject* anOwner)
+	: Component{ anOwner }
+{
+}
+
+C_Stats::~C_Stats()
+{
+}
--- Game/Source/GameObject/Components/Gameplay/Stats/C_Stats.h
+++ Game/Source/GameObject/Components/Gameplay/Stats/C_Stats.h
@@ -1,20 +1,23 @@
 #pragma once
 #include "../../Component.h"
-
+#include "Modifier/StatModifier.hpp"
 
 // OR IntegerStat / FloatStat--??
 
 // C_PhysicalNeeds componetn instead??? -> Stat as ordinary class? 
 
+// templated??
 class C_Stats : public Component
 {
 public:
+	C_Stats(GameObject* anOwner);
+	~C_Stats();
 
-
+	void Restore();
 
 
 private:
-	std::unordered_map<std::string, Hi_Engine::MathExpression<float>>		m_floatStats;
-	std::unordered_map<std::string, Hi_Engine::MathExpression<unsigned>>	m_intStats;
+	std::unordered_map<std::string, StatModifier<float>>		m_floatStats;
+	std::unordered_map<std::string, StatModifier<unsigned>>		m_intStats;
 };
 
