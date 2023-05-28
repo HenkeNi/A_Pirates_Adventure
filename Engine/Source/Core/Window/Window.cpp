119cbd455a5fad947740da3578fb9c92fb329081
Henrik Jangefelt <Henke9105@hotmail.com>
2023-05-28 02:24:11 +0200

    Fixed screen clearing for alpha texutres

--- Engine/Source/Core/Window/Window.cpp
+++ Engine/Source/Core/Window/Window.cpp
@@ -55,7 +55,7 @@ namespace Hi_Engine
 	void Window::ClearScreen() const
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	void Window::SwapBuffers() const
