2156a71c500f3313aa71b1ab4e896ef28a5f8a55
Henrik Jangefelt <Henke9105@hotmail.com>
2023-08-05 13:11:03 +0200

    Some new commands

---
+++ Game/Source/Commands/Inventory/CloseInventoryCommand.h
@@ -0,0 +1,8 @@
+#pragma	 once
+#include "../Base/Command.h"
+
+// Combine with OpenInventoryCommand? Or put in same file?
+class CloseInventoryCommand : public Command
+{
+public:
+};
---
+++ Game/Source/Commands/Inventory/OpenInventoryCommand.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../Base/Command.h"
+
+
+class OpenInventoryCommand : public Command
+{
+public:
+
+
+private:
+
+};
