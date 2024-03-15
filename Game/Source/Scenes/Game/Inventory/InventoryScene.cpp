5aeec71bb539328eb8a09ad84bfdeccf1da33a03
Henrik Jangefelt <Henke9105@hotmail.com>
2024-03-15 20:36:04 +0100

    Added some empty functions

--- Game/Source/Scenes/Game/Inventory/InventoryScene.cpp
+++ Game/Source/Scenes/Game/Inventory/InventoryScene.cpp
@@ -1,2 +1,49 @@
 #include "Pch.h"
 #include "InventoryScene.h"
+
+InventoryScene::InventoryScene(SharedContext aContext)
+	: Scene{ aContext }
+{
+}
+
+InventoryScene::~InventoryScene()
+{
+}
+
+void InventoryScene::Update(float aDeltaTime)
+{
+}
+
+void InventoryScene::LateUpdate(float aDeltaTime)
+{
+}
+
+void InventoryScene::Draw() const
+{
+}
+
+void InventoryScene::OnCreated()
+{
+}
+
+void InventoryScene::OnEnter()
+{
+
+	// background image..
+
+	// UI element box...
+	// render slots in grid...
+	// todo. drag and drop
+
+	// player stats
+	// see player character
+		// equip different items...
+
+
+
+
+}
+
+void InventoryScene::OnExit()
+{
+}
--- Game/Source/Scenes/Game/Inventory/InventoryScene.h
+++ Game/Source/Scenes/Game/Inventory/InventoryScene.h
@@ -5,8 +5,14 @@
 class InventoryScene : public Scene
 {
 public:
+	InventoryScene(SharedContext aContext);
+	~InventoryScene();
 
+	void Update(float aDeltaTime)			  override;
+	void LateUpdate(float aDeltaTime)		  override;
+	void Draw()							const override;
 
-private:
-
+	void OnCreated()						  override;
+	void OnEnter()							  override;
+	void OnExit()							  override;
 };
