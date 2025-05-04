f993d51489fd2ca9cdbaa22c5e1089b786149bfd
Henrik Jangefelt <Henke9105@hotmail.com>
2025-05-04 03:18:05 +0200

    Deduction guide for Range.hpp

--- Engine/Source/Core/Math/Range/Range.hpp
+++ Engine/Source/Core/Math/Range/Range.hpp
@@ -10,6 +10,10 @@ namespace Hi_Engine::Math
 	};
 
 	template <typename T>
+	Range(T, T) -> Range<T>;
+
+	// REMAKE? Make more generic..
+	template <typename T>
 	T MapToNewRange(const T& value, const Range<T>& oldRange, const Range<T>& newRange)
 	{
 		T normalized = (value - oldRange.Min) / (oldRange.Max - oldRange.Min);
@@ -23,4 +27,26 @@ namespace Hi_Engine::Math
 	//{
 	//	return value >= range.Min && range <= range.Max;
 	//}
+
+	/* DEDUCTION GUIDE
+	template<typename Iter>
+	struct Range {
+		Iter begin, end;
+	};
+
+	// Deduce value_type from iterator
+	template<typename Iter>
+	Range(Iter, Iter) -> Range<Iter>;
+	*/
+
+	/*
+	template<typename T>
+	struct Wrapper {
+		T value;
+	};
+
+	// Deduce std::vector's inner type
+	template<typename T>
+	Wrapper(std::vector<T>) -> Wrapper<std::vector<T>>;
+	*/
 }
