0b5c78386fcb5c34d3608e9cc043868ca8967bf6
Henrik Jangefelt <Henke9105@hotmail.com>
2025-05-12 23:13:18 +0200

    Updated ComponentManager

--- Engine/Source/ECS/Core/ComponentManager.h
+++ Engine/Source/ECS/Core/ComponentManager.h
@@ -5,7 +5,7 @@
 #include "Logging/Logger.h"
 #include "Utility/TypeTraits.h"
 
-// TODO; add functions for serialize / deserialize all components? NOT HERE!
+
 
 namespace Hi_Engine
 {
@@ -14,38 +14,58 @@ namespace Hi_Engine
 	public:
 		virtual ~IComponentManager() = default;
 
+		// ==================== Component Management ====================
 		virtual void RemoveComponent(EntityID id) = 0;
+		virtual void RemoveComponents(const std::vector<EntityID>& ids) = 0;
 		virtual void RemoveAllComponents() = 0;
+
+		// ==================== Queries ====================
+		[[nodiscard]] virtual std::size_t GetComponentCount() const noexcept = 0;
+		[[nodiscard]] virtual bool HasComponent(EntityID id) const = 0;
 	};
 
 	template <ComponentType T>
 	class ComponentManager : public IComponentManager
 	{
 	public:
-		// pass in component registry class? (ref)
+		// ==================== Construction/Destruction ====================
+		ComponentManager() = default;
+		~ComponentManager() override = default;
 
+		ComponentManager(const ComponentManager&) = delete;
+		ComponentManager(ComponentManager&&) = default;
+
+		ComponentManager& operator=(const ComponentManager&) = delete;
+		ComponentManager& operator=(ComponentManager&&) = default;
+
+		// ==================== Component Operations ====================
 		template <typename... Args>
 		bool AddComponent(EntityID id, Args&&... args);
 
 		void RemoveComponent(EntityID id) override;
-
+		void RemoveComponents(const std::vector<EntityID>& ids) override;
 		void RemoveAllComponents() override;
 
-		decltype(auto) GetComponent(EntityID id) const;
+		// ==================== Component Access ====================
+		[[nodiscard]] const T& GetComponent(EntityID id) const;
+		[[nodiscard]] T& GetComponent(EntityID id);
 
-		decltype(auto) GetComponent(EntityID id);
+		[[nodiscard]] const T* TryGetComponent(EntityID id) const;
+		[[nodiscard]] T* TryGetComponent(EntityID id);
 
-		decltype(auto) GetComponents() const; // remvoe? or return the sparse set instead?
+		// ==================== Container Access ====================
+		[[nodiscard]] const ComponentContainer<T>& GetContainer() const;
+		[[nodiscard]] ComponentContainer<T>& GetContainer();
 
-		decltype(auto) GetComponents();
+		// ==================== Capacity Management ====================
+		void Reserve(std::size_t capacity);
 
-		const ComponentContainer<T>& GetSparseSet() const { return m_components; }
-
-		ComponentContainer<T>& GetSparseSet() { return m_components; }
+		// ==================== Query Operations ====================
+		[[nodiscard]] std::size_t GetComponentCount() override noexcept;
+		[[nodiscard]] bool HasComponent(EntityID id) const override;
 
 	private:
 		ComponentContainer<T> m_components;
-		//SparseSet<std::decay_t<T>, EntityID> m_components;
 	};
 
 #pragma region Templated_Methods
@@ -64,33 +84,72 @@ namespace Hi_Engine
 	}
 
 	template <ComponentType T>
+	void ComponentManager<T>::RemoveComponents(const std::vector<EntityID>& ids)
+	{
+		for (const auto& id : ids)
+		{
+			m_components.Remove(id);
+		}
+	}
+
+	template <ComponentType T>
 	void ComponentManager<T>::RemoveAllComponents()
 	{
 		m_components.Clear();
 	}
 
 	template <ComponentType T>
-	decltype(auto) ComponentManager<T>::GetComponent(EntityID id) const
+	const T& ComponentManager<T>::GetComponent(EntityID id) const
+	{
+		return m_components.At(id);
+	}
+
+	template <ComponentType T>
+	T& ComponentManager<T>::GetComponent(EntityID id)
+	{
+		return m_components.At(id);
+	}
+
+	template <ComponentType T>
+	const T* ComponentManager<T>::TryGetComponent(EntityID id) const
 	{
 		return m_components.Get(id);
 	}
 
 	template <ComponentType T>
-	decltype(auto) ComponentManager<T>::GetComponent(EntityID id)
+	T* ComponentManager<T>::TryGetComponent(EntityID id)
 	{
 		return m_components.Get(id);
 	}
 
 	template <ComponentType T>
-	decltype(auto) ComponentManager<T>::GetComponents() const
+	const ComponentContainer<T>& ComponentManager<T>::GetContainer() const
 	{
-		return m_components.Data(); // return sparse set?
+		return m_components;
 	}
 
 	template <ComponentType T>
-	decltype(auto) ComponentManager<T>::GetComponents()
+	ComponentContainer<T>& ComponentManager<T>::GetContainer()
 	{
-		return m_components.Data();
+		return m_components;
+	}
+
+	template <ComponentType T>
+	void ComponentManager<T>::Reserve(std::size_t capacity)
+	{
+		m_components.Reserve(capacity);
+	}
+
+	template<ComponentType T>
+	inline std::size_t ComponentManager<T>::GetComponentCount() noexcept
+	{
+		return m_components.Size();
+	}
+
+	template <ComponentType T>
+	bool ComponentManager<T>::HasComponent(EntityID id) const
+	{
+		return m_components.Contains(entity);
 	}
 
 #pragma endregion
