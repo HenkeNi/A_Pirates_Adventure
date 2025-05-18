203d04d92682257302baa8636380044e36115f79
Henrik Jangefelt <Henke9105@hotmail.com>
2025-05-18 21:48:04 +0200

    Changes to Application interface

--- Engine/Source/Core/Application/Application.cpp
+++ Engine/Source/Core/Application/Application.cpp
@@ -1,2 +1,15 @@
 #include "Pch.h"
 #include "Application.h"
+
+namespace Hi_Engine
+{
+	Application::Application()
+		: m_serviceRegistry{ nullptr }
+	{
+	}
+
+	void Application::Initialize(ServiceRegistry* registry)
+	{
+		m_serviceRegistry = registry;
+	}
+}
--- Engine/Source/Core/Application/Application.h
+++ Engine/Source/Core/Application/Application.h
@@ -1,15 +1,26 @@
 #pragma once
+#include "Utility/NonCopyable.h"
 
 namespace Hi_Engine
 {
-	class Application
+	class ServiceRegistry;
+
+	class Application : public NonCopyable
 	{
 	public:
+		Application();
 		virtual ~Application() = default;
 
+		Application(Application&&) = delete;
+		Application& operator=(Application&&) = delete;
+
+		void Initialize(ServiceRegistry* registry);
 		virtual void OnUpdate(float deltaTime)		= 0;
 
 		virtual void OnCreate()						 {};
 		virtual void OnDestroy()					 {};
+
+	protected:
+		ServiceRegistry* m_serviceRegistry;
 	};
 }
