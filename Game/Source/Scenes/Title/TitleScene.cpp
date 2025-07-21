19660472aad804f59a594a31be41a08b54f3dbab
Henrik Jangefelt <Henke9105@hotmail.com>
2025-07-21 23:46:46 +0200

    Clearing systems when exiting title scene

--- Game/Source/Scenes/Title/TitleScene.cpp
+++ Game/Source/Scenes/Title/TitleScene.cpp
@@ -8,15 +8,19 @@ TitleScene::TitleScene(Hi_Engine::ServiceRegistry& registry)
 
 void TitleScene::OnUpdate(float deltaTime)
 {
+	int x = 20;
 }
 
 void TitleScene::OnEnter()
 {
+ 	int x = 20;
 }
 
 void TitleScene::OnExit() 
 {
 	m_world.DestroyAllEntities();
 
-	// Destroy all systems? or do in Scene?
+
+	// Destroy all systems?
+	m_world.RemoveAllSystems();
 }
