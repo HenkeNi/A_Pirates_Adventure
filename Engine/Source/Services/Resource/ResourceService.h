44b1a6c49a6b0cbb99e958065a3db9efedeba968
Henrik Jangefelt <Henke9105@hotmail.com>
2025-07-21 23:53:10 +0200

    Resource Service changes

--- Engine/Source/Services/Resource/ResourceService.h
+++ Engine/Source/Services/Resource/ResourceService.h
@@ -5,133 +5,136 @@ namespace Hi_Engine
 {
 	// TODO; improve this class...
 	// AssetService
-	template <class Resource, typename Identifier = std::string>
+	template <typename ID, typename Res>
 	class ResourceService : public IService
 	{
 	public:		
+		template <typename... Args>
+		Res& Emplace(ID id, Args&&... args);
 
-		[[nodiscard]] const Resource& Get(const Identifier& id) const;
+		void Insert(ID id, std::shared_ptr<Res> resource);
 
-		[[nodiscard]] Resource& Get(const Identifier& id);
+		[[nodiscard]] const Res& Get(const ID& id) const;
+
+		[[nodiscard]] Res& Get(const ID& id);
 		
-		[[nodiscard]] std::shared_ptr<const Resource> TryGetShared(const Identifier& id) const;
+		[[nodiscard]] std::shared_ptr<const Res> TryGetShared(const ID& id) const;
 
-		[[nodiscard]] std::shared_ptr<Resource> TryGetShared(const Identifier& id);
+		[[nodiscard]] std::shared_ptr<Res> TryGetShared(const ID& id);
 
-		[[nodiscard]] std::weak_ptr<const Resource> TryGetWeak(const Identifier& id) const;
+		[[nodiscard]] std::weak_ptr<const Res> TryGetWeak(const ID& id) const;
 
-		[[nodiscard]] std::weak_ptr<Resource> TryGetWeak(const Identifier& id);
+		[[nodiscard]] std::weak_ptr<Res> TryGetWeak(const ID& id);
 
-		std::shared_ptr<Resource> GetResource(const Identifier& identifier);					// return pointer instead
-		const std::shared_ptr<Resource>& GetResource(const Identifier& identifier) const;
-		bool HasResource(const Identifier& identifier) const;
+		[[nodiscard]] std::shared_ptr<Res> GetResource(const ID& id);					// return pointer instead
+		[[nodiscard]] const std::shared_ptr<Res>& GetResource(const ID& id) const;
+		
+		[[nodiscard]] bool Contains(const ID& id) const; // rename Contains?
 
 		//void LoadResources(const std::string& filePath);
-		void Insert(Identifier identifier, std::shared_ptr<Resource> resource);
 		void Clear();
 
 	private:
-		std::unordered_map<Identifier, std::shared_ptr<Resource>> m_resources;
+		std::unordered_map<ID, std::shared_ptr<Res>> m_resources;
 	};
 
-	/*using TextureHolder = ResourceHolder<Texture2D>;
-	using ShaderHolder  = ResourceHolder<GLSLShader>;*/
+#pragma region Templated_Methods
 
-#pragma region Method_Definitions
-
-	template <class Resource, typename Identifier>
-	const Resource& ResourceService<Resource, Identifier>::Get(const Identifier& id) const
+	template <typename ID, typename Res>
+	template <typename ...Args>
+	Res& ResourceService<ID, Res>::Emplace(ID id, Args&&... args)
 	{
-		return {};
-		// TODO: insert return statement here
+		assert(!Contains(id) && "Resource already exist!");
+
+		auto [it, success] = m_resources.insert({ id, std::make_shared<Res>(std::forward<Args>(args)...) });
+
+		if (success)
+		{
+			return it->second;
+		}
+
+		throw std::runtime_error("Failed to create resource!");
+		return Res{};
 	}
 
-	template <class Resource, typename Identifier>
-	Resource& ResourceService<Resource, Identifier>::Get(const Identifier& id)
+	template <typename ID, typename Res>
+	void ResourceService<ID, Res>::Insert(ID id, std::shared_ptr<Res> resource)
 	{
-		return {};
-		// TODO: insert return statement here
-	}
-
-	template <class Resource, typename Identifier>
-	std::shared_ptr<const Resource> ResourceService<Resource, Identifier>::TryGetShared(const Identifier& id) const
-	{
-		return std::shared_ptr<const Resource>();
-	}
-
-	template <class Resource, typename Identifier>
-	std::shared_ptr<Resource> ResourceService<Resource, Identifier>::TryGetShared(const Identifier& id)
-	{
-		return std::shared_ptr<Resource>();
-	}
-
-	template <class Resource, typename Identifier>
-	std::weak_ptr<const Resource> ResourceService<Resource, Identifier>::TryGetWeak(const Identifier& id) const
-	{
-		return std::weak_ptr<const Resource>();
-	}
-
-	template <class Resource, typename Identifier>
-	std::weak_ptr<Resource> ResourceService<Resource, Identifier>::TryGetWeak(const Identifier& id)
-	{
-		return std::weak_ptr<Resource>();
-	}
-
-	template <class Resource, typename Identifier>
-	std::shared_ptr<Resource> ResourceService<Resource, Identifier>::GetResource(const Identifier& identifier)
-	{
-		assert(HasResource(identifier) && "[ResourceHolder::GetResource] - Resource not found");
-		return m_resources.find(identifier)->second;
-	}
-
-	template <class Resource, typename Identifier>
-	const std::shared_ptr<Resource>& ResourceService<Resource, Identifier>::GetResource(const Identifier& identifier) const
-	{
-		assert(HasResource(identifier) && "[ResourceHolder::GetResource] - Resource not found");
-		return m_resources.find(identifier)->second;
-	}
-
-	template <class Resource, typename Identifier>
-	bool ResourceService<Resource, Identifier>::HasResource(const Identifier& identifier) const
-	{
-		auto found = m_resources.find(identifier);
-		return found != m_resources.end();
-	}
-
-	//template <class Resource, typename Identifier>
-	//void ResourceService<Resource, Identifier>::LoadResources(const std::string& filePath)
-	//{
-	//	std::ifstream ifs{ filePath };
-	//	std::string content{ std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>() };
-
-	//	::rapidjson::Document document;
-	//	document.Parse(content.c_str());
-
-	//	assert(!document.HasParseError() && "Failed to parse resource!");
-
-	//	for (auto& value : document.GetArray())
-	//	{
-	//		auto resource = std::make_unique<Resource>();
-	//		assert(resource && "Failed to create resource");
-	//		resource->Init(value);
-
-	//		std::string name = resource->GetName();
-	//		Insert(name, std::move(resource));
-	//	}
-	//}
-
-	template <class Resource, typename Identifier>
-	void ResourceService<Resource, Identifier>::Insert(Identifier identifier, std::shared_ptr<Resource> resource)
-	{
-		auto inserted = m_resources.insert(std::make_pair(identifier, std::move(resource)));
+		auto inserted = m_resources.insert(std::make_pair(id, std::move(resource)));
 		assert(inserted.second);
 	}
 
-	template <class Resource, typename Identifier>
-	void ResourceService<Resource, Identifier>::Clear()
+	template <typename ID, typename Res>
+	const Res& ResourceService<ID, Res>::Get(const ID& id) const
+	{
+		//assert(Contains(id) && "Resource not found!");
+
+		if (auto it = m_resources.find(id); it != m_resources.end())
+		{
+			return *it->second;
+		}
+
+		assert(false && "Resource not found!");
+		return {};
+	}
+
+	template <typename ID, typename Res>
+	Res& ResourceService<ID, Res>::Get(const ID& id)
+	{
+		return {};
+		// TODO: insert return statement here
+	}
+
+	template <typename ID, typename Res>
+	std::shared_ptr<const Res> ResourceService<ID, Res>::TryGetShared(const ID& id) const
+	{
+		return std::shared_ptr<const Res>();
+	}
+
+	template <typename ID, typename Res>
+	std::shared_ptr<Res> ResourceService<ID, Res>::TryGetShared(const ID& id)
+	{
+		return std::shared_ptr<Res>();
+	}
+
+	template <typename ID, typename Res>
+	std::weak_ptr<const Res> ResourceService<ID, Res>::TryGetWeak(const ID& id) const
+	{
+		return std::weak_ptr<const Res>();
+	}
+
+	template <typename ID, typename Res>
+	std::weak_ptr<Res> ResourceService<ID, Res>::TryGetWeak(const ID& id)
+	{
+		return std::weak_ptr<Res>();
+	}
+
+	template <typename ID, typename Res>
+	std::shared_ptr<Res> ResourceService<ID, Res>::GetResource(const ID& id)
+	{
+		assert(Contains(id) && "[ResourceHolder::GetResource] - Resource not found");
+		return m_resources.find(id)->second;
+	}
+
+	template <typename ID, typename Res>
+	const std::shared_ptr<Res>& ResourceService<ID, Res>::GetResource(const ID& id) const
+	{
+		assert(Contains(id) && "[ResourceHolder::GetResource] - Resource not found");
+		return m_resources.find(id)->second;
+	}
+
+	template <typename ID, typename Res>
+	bool ResourceService<ID, Res>::Contains(const ID& id) const
+	{
+		auto found = m_resources.find(id);
+		return found != m_resources.end();
+	}
+
+	template <typename ID, typename Res>
+	void ResourceService<ID, Res>::Clear()
 	{
 		m_resources.clear(); // TODO; delete/unload resources (call function)?
 	}
 
-#pragma endregion Method_Definitions
+#pragma endregion
 }
